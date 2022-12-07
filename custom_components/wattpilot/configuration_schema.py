06ce5fcd76644b135810fc07cc749b4a943d8662
mk-maddin <46523240+mk-maddin@users.noreply.github.com>
2022-12-07 10:00:51 +0100

    Fix options flow / configure button

--- custom_components/wattpilot/configuration_schema.py
+++ custom_components/wattpilot/configuration_schema.py
@@ -1,5 +1,6 @@
 """Configuration schema for Fronius Wattpilot."""
 
+from __future__ import annotations
 from typing import Final
 import logging
 import asyncio
@@ -15,16 +16,32 @@ from homeassistant.const import (
 import homeassistant.helpers.config_validation as cv
 
 from .const import (
+    DEFAULT_NAME,
     DEFAULT_TIMEOUT,
     DOMAIN,
 )
 
-#_LOGGER: Final = logging.getLogger(__name__)
+_LOGGER: Final = logging.getLogger(__name__)
 
 CHARGER_SCHEMA: Final = vol.Schema({
-    vol.Required(CONF_FRIENDLY_NAME, default="Wattpilot"): cv.string,
-    vol.Required(CONF_IP_ADDRESS, default="192.168.1.123"): cv.string,
-    vol.Required(CONF_PASSWORD, default='12345678ABCD'): cv.string,
+    vol.Required(CONF_FRIENDLY_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Required(CONF_IP_ADDRESS, default=None): cv.string,
+    vol.Required(CONF_PASSWORD, default=None): cv.string,
     vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
 })
 
+async def async_get_OPTIONS_CHARGER_SCHEMA(current_data):
+    """Async: return an schema object with current values as default""" 
+    try:
+        _LOGGER.debug("%s - async_get_OPTIONS_CHARGER_SCHEMA", DOMAIN)
+        OPTIONS_CHARGER_SCHEMA: Final = vol.Schema({
+            vol.Required(CONF_FRIENDLY_NAME, default=current_data.get(CONF_FRIENDLY_NAME,DEFAULT_NAME)): cv.string,
+            vol.Required(CONF_IP_ADDRESS, default=current_data.get(CONF_IP_ADDRESS)): cv.string,
+            vol.Required(CONF_PASSWORD, default=current_data.get(CONF_PASSWORD)): cv.string,
+            vol.Optional(CONF_TIMEOUT, default=current_data.get(CONF_TIMEOUT,DEFAULT_TIMEOUT)): cv.positive_int,
+        })
+        await asyncio.sleep(0)
+        return OPTIONS_CHARGER_SCHEMA
+    except Exception as e:
+        _LOGGER.error("%s - async_get_OPTIONS_CHARGER_SCHEMA: failed: %s (%s.%s)", DOMAIN, str(e), e.__class__.__module__, type(e).__name__)
+        return CHARGER_SCHEMA
